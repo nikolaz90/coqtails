54346635e0a1ba03279e7387931c3a4c11002b85
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2025-04-06 21:08:00 +0200

    fix random coktail link

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,43 +1,43 @@
-import React, {useState, useEffect, useCallback} from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import SearchBar from '../components/searchbar'
-import Cocktaillist from '../components/cocktaillist' 
-import {Link} from 'react-router-dom'
+import Cocktaillist from '../components/cocktaillist'
+import { Link } from 'react-router-dom'
 
 
 function Home() {
-  const[random, setRandom] = useState('');
+  const [random, setRandom] = useState('');
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-  const fetchRandomID = useCallback(async ()=>{
-    try{
+  const fetchRandomID = useCallback(async () => {
+    try {
       const response = await fetch(url);
-      const data = await response.json(); 
-      if(data.drinks){
+      const data = await response.json();
+      if (data.drinks) {
         setRandom(data.drinks[0].idDrink)
       } else {
         console.log('no drinks')
       }
-    } catch(error){
+    } catch (error) {
       console.log(error, 'error')
     }
 
-  },[])
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchRandomID();
   }, [])
-  
+
 
   return (
     <section className='section'>
       <h2>Home</h2>
-      <SearchBar/>
+      <SearchBar />
       <div className='random-container'>
         <span className='random-span'>Or chose a random cocktail </span>
-        <Link to={`pers_projects/coqtails/singlecocktail${random}`} className='btn-random'>random cocktail</Link>
+        <Link to={`singlecocktail${random}`} className='btn-random'>random cocktail</Link>
       </div>
-      <Cocktaillist/>
+      <Cocktaillist />
     </section>
-    
+
   )
 }
 
